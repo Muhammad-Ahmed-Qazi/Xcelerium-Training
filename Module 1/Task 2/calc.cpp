b34f1bf3b62c69c55e60184a0f960122465f538a
Muhammad-Ahmed-Qazi <qmuhammadahmed@gmail.com>
2025-11-04 15:01:11 +0500

    Added a while loop with consent to terminate calculate program.

--- Module 1/Task 2/calc.cpp
+++ Module 1/Task 2/calc.cpp
@@ -6,33 +6,42 @@ using namespace std;
 int main() {
 
 	float num1, num2, result;
-	char operation;
+	char operation, consent;
 
-	cout << "Enter first operand: ";
-	cin >> num1;
+	consent = true;
 
-	cout << "Enter second operand: ";
-	cin >> num2;
+	while (consent != 'N' || consent != 'n') {
 
-	cout << "Choose operation: (+, -, *, /): ";
-	cin >> operation;
+		cout << "\n"  << "Enter first operand: ";
+		cin >> num1;
 
-	switch (operation) {
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-			result = num1 / num2;
-			break;
+		cout << "Enter second operand: ";
+		cin >> num2;
+
+		cout << "Choose operation: (+, -, *, /): ";
+		cin >> operation;
+
+		switch (operation) {
+			case '+':
+				result = num1 + num2;
+				break;
+			case '-':
+				result = num1 - num2;
+				break;
+			case '*':
+				result = num1 * num2;
+				break;
+			case '/':
+				result = num1 / num2;
+				break;
+		}
+
+		cout << "\n" << num1 << " " << operation << " " << num2 << " "  << "=" << " " << result << "\n\n";
+
+		cout << "Press N/n to quit or anything else to continue: ";
+		cin >> consent;
+
 	}
-
-	cout << "\n" << num1 << " " << operation << " " << num2 << " "  << "=" << " " << result << "\n\n";
 
 	return 0;
 }
